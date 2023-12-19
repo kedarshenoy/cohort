4a0d74bea1b189d79992b5488afda7080e55b111
kedarshenoy <akedarshenoy@gmail.com>
2023-12-19 21:37:33 +0530

    changes

--- class2.5/index.js
+++ class2.5/index.js
@@ -1,6 +1,8 @@
 const express =require("express");
 const app= express();
-
+const jwt = require("jsonwebtoken");
+const jwtPassword = "123456";
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send(`hello world!`);
